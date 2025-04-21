9b25522c5618944124dca2f00b45b253e906cff8
Игорь Ергин <gora.ergin@gmail.com>
2025-04-21 18:38:01 +0500

    8_Security

---
+++ src/main/java/Ergin/demo/dto/UserDTO.java
@@ -0,0 +1,14 @@
+package Ergin.demo.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UserDTO {
+    String username;
+    String role;
+}
