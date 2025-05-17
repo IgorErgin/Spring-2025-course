9ed2934553bf959aa78abc1af5d0617230118173
Игорь Ергин <gora.ergin@gmail.com>
2025-05-17 15:44:07 +0500

    ДЗ 12

--- src/main/java/Ergin/demo/actuator/TimeLogEndpoint.java
+++ src/main/java/Ergin/demo/actuator/TimeLogEndpoint.java
@@ -1,5 +1,12 @@
 package Ergin.demo.actuator;
 
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
 @Component
 @Endpoint(id = "logtime")
 public class TimeLogEndpoint {
